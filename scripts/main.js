9e05ce670899419fb57fed96078b6696f00ea180
Jaclyn Stutz-Burrick <120345436+JaclynSB@users.noreply.github.com>
2023-09-17 17:12:11 -0700

    adding landing_page and updating website

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+const myHeading = document.querySelector("h1");
+
+
+let x = 2+2
+
+function favoriteAnimal(animal) {
+    return animal + " is my favorite animal!"
+  }
+ 
+  myHeading.textContent = "Hello world!";
+  console.log(favoriteAnimal('x'))
